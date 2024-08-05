9c6e1b49d71e8e052b212a6509daba8aa7de36c0
Ricardo García Jiménez <ricardogj08@riseup.net>
2024-08-05 01:11:14 -0600

    feat: Agrega favicon de la aplicación
    Signed-off-by: Ricardo García Jiménez <ricardogj08@riseup.net>

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= esc($this->renderSection('title')) ?></title>
     <link rel="stylesheet" href="<?= base_url('css/styles.css') ?>">
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🦫</text></svg>"></link>
 </head>
 <body class="bg-base-200">
     <!-- Plantilla base para todas las plantillas -->
